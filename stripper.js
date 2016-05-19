2a6dd602c2f9c7e1dc9fc1caa03a9b7374d325b6
Alistair Dutton <kelveden@gmail.com>
2016-05-19 13:23:24 +0100

    Scrape stripe keys from env vars to allow for pushing to github.

--- stripper.js
+++ stripper.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+const fs = require('fs');
 const app = express();
+
+const stripeSecret = process.env.STRIPE_SK;
+const stripePublic = process.env.STRIPE_PK;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
-app.post('/charge', function (req, res) {
+console.log("Secret key: ", stripeSecret);
+console.log("Public key: ", stripePublic);
+
+app.get('/', (req, res) => {
+    const templatePath = path.normalize("./public/index.html");
+
+    fs.readFile(templatePath, { encoding: "utf8"}, (err, content) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(content.toString("utf8").replace("STRIPE_PK", stripePublic));
+        }
+    });
+});
+
+app.post('/charge', (req, res) => {
     console.log("Request body:");
     console.log(req.body);
 
@@ -14,9 +34,7 @@ app.post('/charge', function (req, res) {
     const amount = parseFloat(req.body.amount);
     const currency = req.body.currency;
 
-    var stripe = require("stripe")(
-        "sk_test_Y1jsEyKcPYWpDRGAVgAs4ksY"
-    );
+    var stripe = require("stripe")(stripeSecret);
 
     stripe.charges.create({
         amount: amount,
